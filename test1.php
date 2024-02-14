66f073403bfe0ddf61e9f80e7aeda4ca307e502f
Alejandro Velasquez <avelasquez@i4digital.com>
2024-02-14 12:42:22 -0500

    feat: version 1 palindrome expression validator  function

--- test1.php
+++ test1.php
@@ -2,7 +2,31 @@
 
 function is_palindrome($input) {
     //solve this method that says if the sentence is palindrome or not
+    $firstCharacter = 0;
+    $lastCharacter = strlen($input) - 1;
+    $palindromeResult = 0;
+
+    while ($lastCharacter > $firstCharacter) {
+        if ($input[$firstCharacter] != $input[$lastCharacter]) {
+            $palindromeResult = 1;
+            break;
+        }
+        $firstCharacter++;
+        $lastCharacter--;
+    }
+
+    if (!$palindromeResult == 0) {
+        return false;
+    }  else {
+        return true;
+    }
+   return  $palindromeResult;
 }
+
+
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
 var_dump(is_palindrome($input));
+var_dump(is_palindrome('radar'));
+var_dump(is_palindrome('raton'));
+var_dump(is_palindrome('los'));
