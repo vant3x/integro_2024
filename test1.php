764da08002e1285e00d43f889b9ec827f1d59325
Alejandro Velasquez <avelasquez@i4digital.com>
2024-02-16 12:42:08 -0500

    fix: fixing and change logic to test2.js

--- test1.php
+++ test1.php
@@ -2,7 +2,7 @@
 
 function is_palindrome($input) {
     //solve this method that says if the sentence is palindrome or not
-    $firstCharacter = 0;
+    $firstCharacter = 0;    
     $lastCharacter = strlen($input) - 1;
     $palindromeResult = 0;
 
@@ -22,8 +22,6 @@ function is_palindrome($input) {
     }
    return  $palindromeResult;
 }
-
-
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
 var_dump(is_palindrome($input));
--- test2.js
+++ test2.js
@@ -1,41 +1,38 @@
 var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
 function apples(input) {
-    function sortInBoxes(apples, applesPerBox) {
-        const boxes = [];
-        let i = 0;
-        
-        while (i < apples) {
-          const box = [];
-          for (let j = 0; j < applesPerBox && i < apples; j++) {
-            box.push(i + 1);
-            i++;
-          }
-          boxes.push(box);
-        }
-        
-        return boxes;
-    }
-
-  function sortInPackages(boxes, boxesPerPackage) {
-    const packages = [];
-    
-    for (let i = 0; i < boxes.length; i += boxesPerPackage * 2) {
-      const package = [];
-      for (let j = i; j < i + boxesPerPackage * 2 && j < boxes.length; j += 2) {
-        package.push([boxes[j], boxes[j + 1]]);
-      }
-      packages.push(package);
-    }
-    
-    return packages;
-  }
-
-  function sortApples(apples) {
+  // acá podemos definir las variables para el script 
+  const applesPerBox = 4;
+   const boxesPerPackage = 2;
+   const friends = 2; 
   
+  // calculamos el numero total de cajas y  paquetes
+  const totalBoxes = Math.ceil(input.length / applesPerBox);
+  const packagesPerFriend = Math.ceil(totalBoxes / (boxesPerPackage * friends));
+
+  // Creamos un objeto para almacenar la salida
+  const output = {};
+
+  // Recorremos los amigos y creamos sus paquetes
+  for (let friend = 0; friend < friends; friend++) {
+    output[friend] = []; // Array para los paquetes del amigo
+
+    // Recorremos los paquetes para cada amigo
+    for (let package = 0; package < packagesPerFriend; package++) {
+      output[friend][package] = []; // Array para las cajas del paquete actual
+
+      // Recorremos las cajas en cada paquete
+      for (let box = 0; box < boxesPerPackage; box++) {
+        const startIndex = applesPerBox * (package * boxesPerPackage + friend * packagesPerFriend * boxesPerPackage + box);
+        const endIndex = Math.min(startIndex + applesPerBox, input.length); // Manejamos cajas incompletas
+
+        output[friend][package].push(input.slice(startIndex, endIndex));
+      }
+    }
   }
 
-  return sortApples(input);
+  return output;
+
 }
 
 console.log(apples(input))
