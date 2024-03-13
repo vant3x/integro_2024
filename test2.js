b28550bc12a146a1ccfeb643b740bdcc372407c5
alevante <alejandrovelasquezmolina@gmail.com>
2024-03-13 17:11:59 -0500

    feat: nueva rama para el PR

--- test2.js
+++ test2.js
@@ -1,7 +1,6 @@
 var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
 function apples(input) {
-  // acá podemos definir las variables para el script 
   const applesPerBox = 4;
    const boxesPerPackage = 2;
    const friends = 2; 
@@ -10,21 +9,17 @@ function apples(input) {
   const totalBoxes = Math.ceil(input.length / applesPerBox);
   const packagesPerFriend = Math.ceil(totalBoxes / (boxesPerPackage * friends));
 
-  // Creamos un objeto para almacenar la salida
   const output = {};
 
-  // Recorremos los amigos y creamos sus paquetes
   for (let friend = 0; friend < friends; friend++) {
-    output[friend] = []; // Array para los paquetes del amigo
+    output[friend] = []; 
 
-    // Recorremos los paquetes para cada amigo
     for (let package = 0; package < packagesPerFriend; package++) {
-      output[friend][package] = []; // Array para las cajas del paquete actual
+      output[friend][package] = []; 
 
-      // Recorremos las cajas en cada paquete
       for (let box = 0; box < boxesPerPackage; box++) {
         const startIndex = applesPerBox * (package * boxesPerPackage + friend * packagesPerFriend * boxesPerPackage + box);
-        const endIndex = Math.min(startIndex + applesPerBox, input.length); // Manejamos cajas incompletas
+        const endIndex = Math.min(startIndex + applesPerBox, input.length); 
 
         output[friend][package].push(input.slice(startIndex, endIndex));
       }
